9318546e9bdf3ec02d7e14d31e844f3aa5ca438a
Mike Warren <lajksdgbiu@gmail.com>
2022-11-01 13:18:29 -0500

    added temp datastore class w/ DI, added ef core in mem package

--- AssetManager/Program.cs
+++ AssetManager/Program.cs
@@ -1,7 +1,10 @@
+using AssetManager.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<IDataStore, TempDataStore>();
 
 var app = builder.Build();
 
