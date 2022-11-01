46801bdf517f77076a06afa10d8531f76694aaa5
Mike Warren <lajksdgbiu@gmail.com>
2022-11-01 12:46:47 -0500

    added Person model

---
+++ AssetManager/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace AssetManager.Models
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string ExternalId { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string EmailAddress { get; set; } = null!;
+        public int? RoleId { get; set; }
+    }
+}
