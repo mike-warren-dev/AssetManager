deb3c0963a4596fd29659d6d29eae21cdcfade17
Mike Warren <lajksdgbiu@gmail.com>
2022-11-01 09:33:05 -0500

    initial commit

---
+++ AssetManager/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AssetManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
