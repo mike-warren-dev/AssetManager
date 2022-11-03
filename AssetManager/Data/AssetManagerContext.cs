3e7d09646eb0688298803d18aacaa9c7ed0218e1
Mike Warren <lajksdgbiu@gmail.com>
2022-11-03 14:27:25 -0500

    Added People Edit--wrong branch :(

---
+++ AssetManager/Data/AssetManagerContext.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AssetManager.Models;
+
+namespace AssetManager.Data;
+
+public class AssetManagerContext : DbContext
+{
+    public DbSet<Person> People { get; set; } = null!;
+
+    public AssetManagerContext()
+    {        
+        List<Person> list = new List<Person>()
+        {
+            new Person()
+            {
+                PersonId = 1,
+                FirstName = "Melvin",
+                LastName = "Snyder",
+                Email = "Melvin.Snyder@SnyderCorp.com",
+                RoleId = null
+            },
+            new Person()
+            {
+                PersonId = 2,
+                FirstName = "Kevin",
+                LastName = "Wells",
+                Email = "Kevin.Wells@SnyderCorp.com",
+                RoleId = null
+            },
+            new Person()
+            {
+                PersonId = 3,
+                FirstName = "Martin",
+                LastName = "Hart",
+                Email = "Martin.Hart@SnyderCorp.com",
+                RoleId = null
+            },
+            new Person()
+            {
+                PersonId = 4,
+                FirstName = "Rose",
+                LastName = "O'Reilly",
+                Email = "Rose.OReilly@SnyderCorp.com",
+                RoleId = null
+            },
+           new Person()
+            {
+                PersonId = 5,
+                FirstName = "Ryan",
+                LastName = "Mills",
+                Email = "Ryan.Mills@SnyderCorp.com",
+                RoleId = null
+            },
+        };
+
+        People.AddRange(list);
+    }
+}
---
+++ AssetManager/Repos/PersonRepository.cs
@@ -0,0 +1,56 @@
+﻿using AssetManager.Data;
+using AssetManager.DTOs;
+using AssetManager.Models;
+
+namespace AssetManager.Repos;
+
+public class PersonRepository : IPersonRepository
+{
+    public IDataStore _context;
+
+    public PersonRepository(IDataStore context)
+    {
+        _context = context;
+    }
+
+    public Person? GetPersonById(int id)
+    {
+        return _context.People.FirstOrDefault(p => p.PersonId == id); ;
+    }
+    public void Create(PersonCreateDto submission)
+    {
+        Person person = new()
+        {
+            PersonId = _context.People.Max(p => p.PersonId) + 1,
+            FirstName = submission.FirstName,
+            LastName = submission.LastName,
+            Email = submission.Email,
+            RoleId = submission.RoleId
+        };
+
+        _context.People.Add(person);
+    }
+
+    public void Update(PersonCreateDto submission)
+    {
+        Person? person = _context.People.FirstOrDefault(p => p.PersonId == submission.PersonId);
+
+        if (person != null)
+        {
+            person.PersonId = submission.PersonId;
+            person.FirstName = submission.FirstName;
+            person.LastName = submission.LastName;
+            person.Email = submission.Email;
+            person.RoleId = submission.RoleId;
+
+            int index = _context.People.FindIndex(p => p.PersonId == person.PersonId);
+
+            _context.People[index] = person;
+        }
+    }
+
+    public void Delete()
+    {
+
+    }
+}
