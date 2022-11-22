b813f281f794fb0258b4a4e7554bc2c0248275ac
Mike Warren <warren.michael.douglas@gmail.com>
2022-11-22 17:02:51 -0600

    Add 'x' button to Order modal's Products field, enabling deletion of products / unused fields.

--- AssetManager/Services/IOrderService.cs
+++ AssetManager/Services/IOrderService.cs
@@ -5,10 +5,10 @@ namespace AssetManager.Services
     public interface IOrderService
     {
         List<OrderDisplayDto> GetAllOrders();
-        void Create(OrderAddEditDto submission);
+        void Create(OrderAddEditDto dto);
         void Delete(int id);
         OrderAddEditDto? GetOrderById(int id);
         void ReceiveOrder(int id);
-        void Update(OrderAddEditDto submission);
+        void Update(OrderAddEditDto dto);
     }
 }
