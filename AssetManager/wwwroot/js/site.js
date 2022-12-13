b729ce134f1dd52c436d387e5d0aa8676947a2c9
Mike Warren <warren.michael.douglas@gmail.com>
2022-12-13 12:40:52 -0600

    added alerts on success of all jQuery calls

--- AssetManager/wwwroot/js/site.js
+++ AssetManager/wwwroot/js/site.js
@@ -2,3 +2,54 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function doAnAlert(alertType) {
+
+    var showAlertDuration = 2000;
+    var fadeoutSpeed = "slow";
+
+    switch (alertType) {
+        // Generic
+        case "failure":
+            $("#alertDivRed").text("Something went wrong. Please refresh and try again.").toggle().delay(showAlertDuration).fadeOut(fadeoutSpeed);
+            break;
+
+        // Order
+        case "orderCreateSuccess":
+            $("#alertDivGreen").text("Order created.").toggle().delay(showAlertDuration).fadeOut(fadeoutSpeed);
+            break;
+        case "orderEditSuccess":
+            $("#alertDivGreen").text("Order edited.").toggle().delay(showAlertDuration).fadeOut(fadeoutSpeed);
+            break;
+        case "orderReceiveSuccess":
+            $("#alertDivGreen").text("Order received. Assets Created.").toggle().delay(showAlertDuration).fadeOut(fadeoutSpeed);
+            break;
+        case "orderDeleteSuccess":
+            $("#alertDivGreen").text("Order deleted.").toggle().delay(showAlertDuration).fadeOut(fadeoutSpeed);
+            break;
+
+        // Person        
+        case "personCreateSuccess":
+            $("#alertDivGreen").text("Person created.").toggle().delay(showAlertDuration).fadeOut(fadeoutSpeed);
+            break;
+        case "personEditSuccess":
+            $("#alertDivGreen").text("Person edited.").toggle().delay(showAlertDuration).fadeOut(fadeoutSpeed);
+            break;
+        case "personDeleteSuccess":
+            $("#alertDivGreen").text("Person deleted.").toggle().delay(showAlertDuration).fadeOut(fadeoutSpeed);
+            break;
+
+        //Asset
+        case "assetCreateSuccess":
+            $("#alertDivGreen").text("Asset created.").toggle().delay(showAlertDuration).fadeOut(fadeoutSpeed);
+            break;
+        case "assetEditSuccess":
+            $("#alertDivGreen").text("Asset edited.").toggle().delay(showAlertDuration).fadeOut(fadeoutSpeed);
+            break;
+        case "assetDeleteSuccess":
+            $("#alertDivGreen").text("Asset deleted.").toggle().delay(showAlertDuration).fadeOut(fadeoutSpeed);
+            break;
+    }
+}
+
+
